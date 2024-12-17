9fa4887fbc4ded59aa47e6bcc3ffed7ab4d58dd7
Goziee-git <prospachigoz@gmail.com>
2024-12-17 14:36:02 +0100

    mad-Libs challange in python

--- week4-challange/Mad-Libs.py
+++ week4-challange/Mad-Libs.py
@@ -1,9 +1,50 @@
-noun = input("Enter four(4) nouns seperated by spaces you can think off:")
-noun1, noun2, noun3, noun4 = noun.split()
-adjective = input("Enter two(2) adjectives seperated by spaces here too:")
-adjective1, adjective2 = adjective.split()
-verb = input("type in four(4) verbs here seperatedd by spaces:")
-verb1, verb2, verb3, verb4 = verb.split()
 
-print("the", noun1, "is most certainly a",adjective1,". He really has alot of knowledge about", adjective1, "and is a great", verb1)
-print(noun, "is also a sports lover.", adjective2, "is his favourite recreational activity.", "I really want to hang out and do some", verb2, "with him")
+
+   #User inputs
+name = input("What is your name:")
+age = int(input("How old are you:"))
+city = input("what city do u live in:")
+food = input("what is your favourite food:")
+activity = input("Enter some activity here:")
+
+   #conditional statment based on age
+if age < 12:
+   description = "a young lad"
+elif 12 <= age <= 18:
+   description = " explosive teenager"
+elif 18 <= age <= 25:
+   description = "an early adult"
+else:
+   decription = "an adult of great wisdom"   
+
+   #conditional statements based on food
+if food in ['Jellof Rice', 'Nkwobi', 'Ogbono Soup']:
+   print("You are Nigerian")
+elif food in ['Jambalaya', 'Buffalo wings', 'Lobster rolls']:
+   print("You must be American")
+elif food in ['Kimchi', 'Dim Sim', 'Sushi']:
+   print("You must be asian")
+else:
+   print(f"{food} sounds nice, but i cant say where youre a native of 🥺")
+
+   #conditional statememts based on city
+if city in ['Abuja', 'Lagos', 'Kano', 'Enugu']:
+   description = "a city in Nigeria"
+elif city in ['New York', 'Los Angeles', 'Chicago', 'Miami']:
+   description = "a city in the United States of America"
+elif city in ['Jakarta','Beijing','Fukuoka','Shanghai']:
+   description = 'an asian city'
+else:
+   print (f" {city} sounds cool, but i cant say where it is🦹🏼‍♂️")
+
+   #mad Lib story based on user inputs
+print("\nHere is your story:\n")
+print(f"I've come across someone named {name}, just about {age} years old ,he seems like {description} and was such a joy to be with")
+print(f"often times i get to meet people from {city} and ask them about the {food} there")
+print(f"usually people from {city} would say the love to eat {food} and do some {activity} which is common in those places")
+
+   #conditional statement based on activity
+if activity in ['dancing', 'soccer','meditation']:
+   print(f'everyone in {city} find {activity} aesthetically appealing')
+elif activity in ['Holi','Great wall of China', 'origami']:
+   print (f'asians bravery in {activity} and dedication makes them standout at whatsoever range')
