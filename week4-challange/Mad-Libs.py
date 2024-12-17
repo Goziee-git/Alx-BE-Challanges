76240068f548e938802440c3737426ca5e012e48
Goziee-git <prospachigoz@gmail.com>
2024-12-17 12:34:45 +0100

    challange to create a fun MadLib

---
+++ week4-challange/Mad-Libs.py
@@ -0,0 +1,9 @@
+noun = input("Enter four(4) nouns seperated by spaces you can think off:")
+noun1, noun2, noun3, noun4 = noun.split()
+adjective = input("Enter two(2) adjectives seperated by spaces here too:")
+adjective1, adjective2 = adjective.split()
+verb = input("type in four(4) verbs here seperatedd by spaces:")
+verb1, verb2, verb3, verb4 = verb.split()
+
+print("the", noun1, "is most certainly a",adjective1,". He really has alot of knowledge about", adjective1, "and is a great", verb1)
+print(noun, "is also a sports lover.", adjective2, "is his favourite recreational activity.", "I really want to hang out and do some", verb2, "with him")
